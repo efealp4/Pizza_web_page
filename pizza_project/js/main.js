e38aa866dc47ba290fdbf66bff88bb322b29bd6c
efealp4 <efealp1664@hotmail.com>
2025-03-10 16:49:55 +0300

    Updates
    ...

--- pizza_project/js/main.js
+++ pizza_project/js/main.js
@@ -40,3 +40,11 @@ const swiperPopular= new Swiper('.popular__swiper', {
    slidesPerView: 'auto',
    centeredSlides: 'auto'
 })
+
+/* Show Scroll up*/ 
+const scrollUp= () =>{
+   const scrollUp= document.getElementById('scroll-up');
+   // When the scrollup is higher than 350 viewport height, add the show-scroll class to the a tag with the scrollup class
+   this.scrollY >= 350 ? scrollUp.classList.add('show-scroll') : scrollUp.classList.remove('show-scroll')
+}
+window.addEventListener('scroll',scrollUp);
