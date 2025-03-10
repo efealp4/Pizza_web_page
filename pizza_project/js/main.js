dc33f7c2ec85f0d2f623deae6a80b1514d8d7c8e
efealp4 <efealp1664@hotmail.com>
2025-03-10 21:21:20 +0300

    Upload
    upload

--- pizza_project/js/main.js
+++ pizza_project/js/main.js
@@ -1,9 +1,18 @@
 /* ====== SHOW MENU ====== */ 
+// That part control the hamburger menu. 
 const navMenu= document.getElementById('nav-menu'),
+      // It symbolize to the all of the mobile menu.
       navToggle= document.getElementById('nav-toggle'),
+      // The button that provide the open the menu.
       navClose= document.getElementById('nav-close')
+      // The button that provide the close the menu.
 
-/* Menu Show */ 
+/* Menu Show */
+/* 
+If hamburger menu is visible : 
+* show-menu is adding to the nav-menu when click. 
+* Because of that property, it gets visible in CSS.
+*/ 
 if(navToggle){
    navToggle.addEventListener('click',()=>{
       navMenu.classList.add('show-menu')
@@ -11,15 +20,27 @@ if(navToggle){
 }
 
 /* Menu Hidden */ 
+/* 
+If there is x button on menu: 
+* It take the show-menu off from CSS when click.
+*/ 
 if(navClose){
    navClose.addEventListener('click',()=>{
       navMenu.classList.remove('show-menu')
    })
 }
 
+/*---------------------------------------------*/ 
 /* Remove Menu Mobile */
-const navLink= document.querySelectorAll('.nav-link');
+/* 
+That code blocks provide close the menu if user clicl any link. 
+*/ 
+const navLink= document.querySelectorAll('.nav__link');
 
+/* 
+* Choose the all nav__link elements.
+* By forEach, Run the linkAction when click every each of link. 
+*/ 
 const linkAction = () =>{
    const navMenu= document.getElementById('nav-menu')
    navMenu.classList.remove('show-menu')
@@ -27,6 +48,14 @@ const linkAction = () =>{
 navLink.forEach(n=> n.addEventListener('click', linkAction))
 
 /* Add Shadow Header */ 
+/* 
+That code blocks provide that add a shadow behind the header while scroll down. 
+* window.addEventListener('scroll',shadowHeader): 
+   * It runs when scrolls. 
+* scrollY >= 50: 
+   * It adds the shadowHeader class to the #header.
+   * On the top level, the shadow gets remove. 
+*/ 
 const shadowHeader = ()=>{
    const header= document.getElementById('header')
    this.scrollY >= 50 ? header.classList.add('shadow-header') : header.classList.remove('shadow-header');
@@ -34,17 +63,60 @@ const shadowHeader = ()=>{
 window.addEventListener('scroll',shadowHeader)
 
 /* Swiper popular */ 
+/* 
+That code blocks manage the slider that on the popular pizzas.
+* It uses the librarie of swiper(swiper-bundle.min.js).
+* loop: true -> Slider continue slide infinit. 
+* grabCursor -> Set the cursor. 
+* slidePerView: 'auto' : Showing the screen card as much as avaible.
+* centeredSlides: 'auto' : It gets middle the cards as otomatic. 
+*/ 
 const swiperPopular= new Swiper('.popular__swiper', {
    loop:true,
-   grapCursor: true,
+   grabCursor: true,
    slidesPerView: 'auto',
    centeredSlides: 'auto'
 })
 
 /* Show Scroll up*/ 
+/* 
+That code blocks provide that show the button of scrollUp when scroll down after 350px.
+* ScrollUp function runs while scroll. 
+* If the height of page will be bigger than 350px, show-scroll element gets add to the #scroll-up.
+* It gets unvisible, if scroll up. 
+*/ 
 const scrollUp= () =>{
    const scrollUp= document.getElementById('scroll-up');
    // When the scrollup is higher than 350 viewport height, add the show-scroll class to the a tag with the scrollup class
    this.scrollY >= 350 ? scrollUp.classList.add('show-scroll') : scrollUp.classList.remove('show-scroll')
 }
 window.addEventListener('scroll',scrollUp);
+
+/* Scroll Sections Active Links */
+/* 
+That code blocks provide that highlights the menu links that belong that's sections. 
+* Choose every each elements of id.
+* When scroll: 
+   * Whichever section is on the screen, the active-link class is added to the corresponding menu link.
+   * Gets remove from another ones. 
+*/
+const sections= document.querySelectorAll('section[id]');
+
+const scrollActive= ()=> {
+   const scrollDown= window.scrollY
+  
+   sections.forEach(current => {
+      const sectionHeight= current.offsetHeight,
+      sectionTop = current.offsetTop -58,
+      sectionId= current.getAttribute('id'),
+      sectionsClass= document.querySelector('.nav__menu a[href*=' + sectionId + ']')
+
+      if(scrollDown > sectionTop && scrollDown <= sectionTop + sectionHeight) {
+         sectionsClass.classList.add('active-link')
+      }
+      else{
+         sectionsClass.classList.remove('active-link')
+      }
+   })
+}
+window.addEventListener('scroll',scrollActive)
