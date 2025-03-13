f9e81d593f5d937bebeed414a5100764b14c8e03
efealp4 <efealp1664@hotmail.com>
2025-03-13 17:53:45 +0300

    Final Version
    Final version of website

--- pizza_project/js/main.js
+++ pizza_project/js/main.js
@@ -120,3 +120,20 @@ const scrollActive= ()=> {
    })
 }
 window.addEventListener('scroll',scrollActive)
+
+/* ====== SCROLL REVEAL ANIMATION ====== */ 
+const sr= ScrollReveal({
+   origin: 'top',
+   distance: '60px', 
+   duration: 2500, 
+   delay: 300, 
+   // reset: true, //Animations repeat
+})
+
+sr.reveal('.home__data, .popular__container, .footer, .recipe__img')
+sr.reveal('.home__board', {delay: 700, distance: '100px', origin: 'right'})
+sr.reveal('.home__pizza', {delay: 1400, distance: '100px', origin: 'bottom', rotate: {z: -90}})
+sr.reveal('.home__ingredient', {delay: 1400, interval: 100})
+sr.reveal('.about__data, .recipe__list, .contact__data' ,{origin: 'right'})
+sr.reveal('.about__img, .contact__image' ,{origin: 'left'})
+sr.reveal('.products__card', {interval: 100})
